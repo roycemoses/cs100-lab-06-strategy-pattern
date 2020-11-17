10d611e1ce753f19520c34209e7752ea5e95d62e
roycemoses <royce.kmoses@gmail.com>
2020-11-17 15:40:58 -0800

    Added DefaultConstructorVCSizeTest

--- container_and_sort_tests.hpp
+++ container_and_sort_tests.hpp
@@ -2,7 +2,29 @@
 #define __CONTAINER_AND_SORT_TESTS_HPP__
 
 #include "gtest/gtest.h"
+#include "vector_container.hpp"
+#include "selection_sort.hpp"
 
 
+TEST(VectorContainerTests, DefaultConstructorVCSizeTest) {
+    Container* vc = new VectorContainer();
+    EXPECT_EQ(vc->size(), 0);
+}
+
+
+// TEST(VectorContainerTest, SortAfterDefaultConstructorTest) {
+//     Container* vc = new VectorContainer();
+//     std::string error = "Uninitialized sort_function pointer";
+//     EXPECT_ANY_THROW({
+//         try {
+//             vc->sort();
+//         }
+//         catch (char** str[])
+//         {
+//             EXPECT_STREQ(error, str);
+//             throw;
+//         }
+//     });
+// }
 
 #endif // __CONTAINER_AND_SORT_TESTS_HPP__
