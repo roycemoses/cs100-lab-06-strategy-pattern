060aeeb906eb20c9d31b2b1fd33a440699698653
Danz Moses <danzmoses@gmail.com>
2020-11-16 16:47:34 -0800

    Implemented set_sort_function() for ListContainer class

--- list_container.cpp
+++ list_container.cpp
@@ -7,5 +7,9 @@ ListContainer::ListContainer() : sort_function(nullptr)
 }
 
 /* Non Virtual Functions */
+void ListContainer::set_sort_function(Sort* sort_function)
+{
+    this->sort_function = sort_function;
+}
 
 /* Virtual Functions */
