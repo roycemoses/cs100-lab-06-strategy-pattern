923601b7260bac67d5dbf88b653efee15a4b9426
roycemoses <royce.kmoses@gmail.com>
2020-11-16 17:29:16 -0800

    Fixed sort() function and formatting

--- vector_container.cpp
+++ vector_container.cpp
@@ -13,7 +13,11 @@ void VectorContainer::set_sort_function(Sort* sort_function) { this->sort_functi
 void VectorContainer::add_element(Base* element) { this->v.push_back(element); }
 
 // iterate through trees and output the expressions (use stringify())
-void VectorContainer::print() { for(int i = 0; i < v.size(); ++i) { v.at(i)->stringify(); } }
+void VectorContainer::print() 
+{ 
+    for(int i = 0; i < v.size(); ++i) 
+        v.at(i)->stringify();
+}
 
 // calls on the previously set sorting-algorithm. Checks if sort_function is not null, throw exception if otherwise
 void VectorContainer::sort() 
@@ -28,7 +32,12 @@ void VectorContainer::sort()
 
 /* Essentially the only functions needed to sort */
 //switch tree locations
-void VectorContainer::swap(int i, int j) { Base* temp = v.at(i); v.at(i) = v.at(j); v.at(j) = temp; }
+void VectorContainer::swap(int i, int j) 
+{ 
+    Base* temp = v.at(i); 
+    v.at(i) = v.at(j); 
+    v.at(j) = temp; 
+}
 
 // get top ptr of tree at index i
 Base* VectorContainer::at(int i) { return v.at(i); }
