f1467bf8ce4e65811dd0b1ccfb40aa6c81f4ee8e
Danz Moses <danzmoses@gmail.com>
2020-11-16 16:56:51 -0800

    Implemented ListContainer constructor that takes in a Sort pointer

--- list_container.cpp
+++ list_container.cpp
@@ -6,6 +6,13 @@ ListContainer::ListContainer() : sort_function(nullptr)
     ;
 }
 
+ListContainer::ListContainer(Sort* function) : sort_function(function)
+{
+    sz = 0;
+    capacity = 1;
+    list = new Base*[capacity];
+}
+
 /* Non Virtual Functions */
 void ListContainer::set_sort_function(Sort* sort_function)
 {
