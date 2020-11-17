ac1efb30ef817e57de44177b0f4801d4db8747f1
Danz Moses <danzmoses@gmail.com>
2020-11-16 17:45:57 -0800

    Updated class declaration for BubbleSort class

--- bubble_sort.hpp
+++ bubble_sort.hpp
@@ -1,17 +1,15 @@
 #ifndef __BUBBLE_SORT_HPP__
 #define __BUBBLE_SORT_HPP__
 
-#include "container.hpp"
+#include "sort.hpp"
 
-class Container;
-
-class BubbleSort {
+class BubbleSort : public Sort {
     public:
         /* Constructors */
         BubbleSort();
 
         /* Pure Virtual Functions */
-        virtual void sort(Container* container) = 0;
+        virtual void sort(Container* container);
 };
 
 #endif //__BUBBLE_SORT_HPP__
