f1f7f6ba54ccb8a672c3816486a1bcafe2215b73
roycemoses <royce.kmoses@gmail.com>
2020-11-17 18:26:09 -0800

    Instead of stringify(), use cout

--- vector_container.cpp
+++ vector_container.cpp
@@ -9,9 +9,14 @@ void VectorContainer::add_element(Base* element) { this->v.push_back(element); }
 
 // iterate through trees and output the expressions (use stringify())
 void VectorContainer::print() 
-{ 
-    for(int i = 0; i < v.size(); ++i) 
-        v.at(i)->stringify();
+{
+    for (int i = 0; i < v.size(); ++i) 
+    {
+        std::cout << v.at(i)->stringify();
+        if (i < v.size() - 1)
+            std::cout << ", ";
+    }  
+    std::cout << std::endl;
 }
 
 // calls on the previously set sorting-algorithm. Checks if sort_function is not null, throw exception if otherwise
