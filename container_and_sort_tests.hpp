ef130852b330c9a984c5ebc4bb0635d45123d58e
roycemoses <royce.kmoses@gmail.com>
2020-11-17 19:26:53 -0800

    Added ListContainerSelectionSortTest

--- container_and_sort_tests.hpp
+++ container_and_sort_tests.hpp
@@ -10,6 +10,7 @@
 #include "vector_container.hpp"
 #include "selection_sort.hpp"
 #include "mult.hpp"
+#include "sub.hpp"
 
 
 TEST(ListContainerTests, NoArgumentConstructor)
@@ -236,6 +237,38 @@ TEST(VectorContainerTests, SizeTest) {
     vc->add_element(op2);
 
     EXPECT_EQ(vc->size(), 3);  
+}
+
+TEST(SortTestSet, ListContainerSelectionSortTest) {
+    Op* seven = new Op(7);
+    Op* four = new Op(4);
+    Mult* TreeA = new Mult(seven, four);
+
+    Op* three = new Op(3);
+    Op* two = new Op(2);
+    Add* TreeB = new Add(three, two);
+
+    Op* ten = new Op(10);
+    Op* six = new Op(6);
+    Sub* TreeC = new Sub(ten, six);
+
+    ListContainer* container = new ListContainer();
+    container->add_element(TreeA);
+    container->add_element(TreeB);
+    container->add_element(TreeC);
+
+    ASSERT_EQ(container->size(), 3);
+    EXPECT_EQ(container->at(0)->evaluate(), 28);
+    EXPECT_EQ(container->at(1)->evaluate(), 5);
+    EXPECT_EQ(container->at(2)->evaluate(), 4);
+
+    container->set_sort_function(new SelectionSort());
+    container->sort();
+
+    ASSERT_EQ(container->size(), 3);
+    EXPECT_EQ(container->at(0)->evaluate(), 4);
+    EXPECT_EQ(container->at(1)->evaluate(), 5);
+    EXPECT_EQ(container->at(2)->evaluate(), 28);
 }
 
 #endif // CONTAINER_AND_SORT_TESTS_HPP
